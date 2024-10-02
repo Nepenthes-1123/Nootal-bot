b7371e8a4bcb7a78f886bf640a351c5b2b3420b6
Nepenthes-1123 <108643050+Nepenthes-1123@users.noreply.github.com>
2024-10-03 00:58:02 +0900

    cap len fixed

--- funcs.py
+++ funcs.py
@@ -1,13 +1,12 @@
-from players import Participant
-from teams import Team
 import copy
 import random
 import statistics
 
-def dec_cap(
-        player_list: list[Participant], 
-        TEAM_NUM: int
-        ) -> list[Participant]:
+from players import Participant
+from teams import Team
+
+
+def dec_cap(player_list: list[Participant], TEAM_NUM: int) -> list[Participant]:
     """プレイヤーリスト, チーム数を受け取って主将を決定
     可能なら主将に決定したプレイヤーをplayer_listから除外しておきたい
 
@@ -24,30 +23,38 @@ def dec_cap(
     cap_num: int = 0
     player_list_copy = copy.deepcopy(player_list)
 
-    #主将の人数を数え、cap_listに入れる
+    # 主将の人数を数え、cap_listに入れる
     for i in range(len(player_list_copy)):
-        if player_list_copy[i].captain:
-            p = player_list_copy.pop(i)
+        if player_list[i].captain:
+            p = player_list_copy[i]
             cap_list.append(p)
             cap_num += 1
-    
-    #主将が足りない場合に補充
+
+    for cap in cap_list:
+        player_list_copy.remove(cap)
+
+    # 主将が足りない場合に補充
     while cap_num < TEAM_NUM:
         random_cap: int = random.randint(1, len(player_list_copy))
-        p = player_list_copy.pop(random_cap)
-        p.captain =  True
+        p = player_list_copy[random_cap]
+        p.captain = True
         cap_list.append(p)
         cap_num += 1
 
-    #主将を除いたplayer_listも返せます
-    #return (player_list_copy, cap_list)
+    for cap in cap_list:
+        if cap in player_list_copy:
+            player_list_copy.remove(cap)
+
+    player_list = player_list_copy
+
+    # 主将を除いたplayer_listも返せます
+    # return (player_list_copy, cap_list)
     return cap_list
 
 
 def dec_team_rand(
-        player_list: list[Participant], 
-        teams: list[Team]
-        ) -> list[Participant]:
+    player_list: list[Participant], teams: list[Team]
+) -> list[Participant]:
     """ランダムにチームを決定するアルゴリズム
 
     Args:
@@ -56,19 +63,20 @@ def dec_team_rand(
     Returns:
         list[Participant]: 決定後のチームリスト
     """
-    #teamのエリアpwの平均を返す関数
-    def team_mean_func(team: list) -> (int, int):
+
+    # teamのエリアpwの平均を返す関数
+    def team_mean_func(team: list) -> tuple[int, int]:
         pw_list = [team[i].zones_pw for i in range(len(team))]
         return statistics.mean(pw_list)
-    
-    par_num: int = len(player_list) #主将を除いた参加人数
+
+    par_num: int = len(player_list)  # 主将を除いた参加人数
     teams_num: int = len(teams)
-    mean_var_max: float = 10**8 #全チームの平均の分散
-    m: int = 10**4 #繰り返し調べる回数
-    teams_keep = [] #ずっと保存
+    mean_var_max: float = 10**8  # 全チームの平均の分散
+    m: int = 10**4  # 繰り返し調べる回数
+    teams_keep = []  # ずっと保存
 
     for i in range(m):
-        teams_keeping = copy.deepcopy(teams) #一時保存
+        teams_keeping = copy.deepcopy(teams)  # 一時保存
         shuffled_data = random.sample(player_list)
         player_shuffle_list = [shuffled_data[i::teams_num] for i in range(teams_num)]
         mean_list = []
